3abb3af378224c34f7d44d4faaed0d07e3bddfa3
ExeN93 <exen315@gmail.com>
2025-06-04 15:41:31 +0200

    feat: register page

--- prototype/znanyFryzjer/src/App.tsx
+++ prototype/znanyFryzjer/src/App.tsx
@@ -2,12 +2,14 @@ import { MantineProvider } from '@mantine/core'
 import { Route, Routes } from 'react-router-dom'
 import { LoginPage } from './pages/login'
 import { theme } from './theme'
+import { RegisterPage } from './pages/register'
 
 function App() {
 	return (
 		<MantineProvider theme={theme}>
 			<Routes>
-				<Route path='/login' element={<LoginPage />} />
+				<Route path='/' element={<LoginPage />} />
+				<Route path='/register' element={<RegisterPage />} />
 			</Routes>
 		</MantineProvider>
 	)
---
+++ prototype/znanyFryzjer/src/pages/register.tsx
@@ -0,0 +1,61 @@
+import {
+    Anchor,
+    Box,
+    Button,
+    Center,
+    Container,
+    Flex,
+    Group,
+    Paper,
+    PasswordInput,
+    Stack,
+    Text,
+    TextInput,
+    Title,
+} from '@mantine/core'
+import type React from 'react'
+import {  useNavigate } from 'react-router-dom'
+
+export const RegisterPage: React.FC = () => {
+    const navigate = useNavigate()
+    return (
+        <Container size="xs">
+            <Center mih="100vh">
+                <Stack w="100%" mih="100vh" justify="space-between">
+                    <Box h="30px"></Box>
+                    <Group w="100%">
+                        <Center w="100%" mb="lg">
+                            <Title order={1}>Znany Fryzjer</Title>
+                        </Center>
+                        <Paper withBorder p="xl" radius="md" w="100%">
+                            <Stack gap="md">
+                                <Center>
+                                    <Text size="xl" fw={700}>
+                                        Rejestracja
+                                    </Text>
+                                </Center>
+                                <TextInput label="Imię" placeholder="Imię" required />
+                                <TextInput label="Email" placeholder="Email" required />
+                                <PasswordInput label="Hasło" placeholder="Hasło" required />
+                                <Button fullWidth mt="md">
+                                    Zarejestruj się
+                                </Button>
+                                <Flex justify={'center'} align={'center'} pt={'md'}>
+                                    <Text>Masz konto?</Text>
+                                    <Text style={{ cursor: 'pointer', textDecoration: 'underline' }} ml="xs" variant="subtle" onClick={() => navigate('/')} c="teal.6">
+                                        Zaloguj się
+                                    </Text>
+                                </Flex>
+                            </Stack>
+                        </Paper>
+                    </Group>
+                    <Box h="30px" bg="teal.6">
+                        <Text c="dimmed" size="xs">
+                            Opracowano przez polskich studentów
+                        </Text>
+                    </Box>
+                </Stack>
+            </Center>
+        </Container>
+    )
+}
